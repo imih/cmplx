2fea455021b945263ee9e4316fe638823aba96d0
Iva <ivamiho@gmail.com>
2016-04-26 21:23:21 +0200

    building

--- common/igraph.cc
+++ common/igraph.cc
@@ -19,6 +19,74 @@ IGraph *IGraph::UndirectedLattice(const std::vector<int> dimensions) {
   return new IGraph(graph);
 }
 
+IGraph *IGraph::BarabasiAlbert(int nodes) {
+  igraph_t *graph = (igraph_t *)malloc(sizeof(igraph_t));
+  igraph_barabasi_game(graph, nodes, 1, 2, NULL, 0, 1, IGRAPH_UNDIRECTED,
+                       IGRAPH_BARABASI_BAG, NULL);
+  return new IGraph(graph);
+}
+
+IGraph *IGraph::ErdosRenyi(int nodes, double p) {
+  igraph_t *graph = (igraph_t *)malloc(sizeof(igraph_t));
+  igraph_erdos_renyi_game(graph, IGRAPH_ERDOS_RENYI_GNP, nodes, p,
+                          IGRAPH_UNDIRECTED, 0 /* loops*/);
+  return new IGraph(graph);
+}
+
+bool IGraph::is_connected() const {
+  igraph_bool_t bool_t;
+  igraph_is_connected(graph_, &bool_t, IGRAPH_WEAK);
+  return (bool) bool_t;
+}
+
+int IGraph::kCore(int node_id) const {
+  if (cores_.get()->empty()) {
+    igraph_coreness(graph_, cores_.get()->vector(),
+                    IGRAPH_ALL /* undirected */);
+  }
+  return (*cores_.get())[node_id];
+}
+
+double IGraph::closeness(int node_id) const {
+  if (closeness_.get()->empty()) {
+    igraph_vs_t *vs_t = (igraph_vs_t *)malloc(sizeof(igraph_vs_t));
+    igraph_vs_all(vs_t);
+    igraph_closeness(graph_, closeness_.get()->vector(), *vs_t, IGRAPH_ALL,
+                     NULL, true /* normalize*/);
+    delete vs_t;
+  }
+  return (*closeness_.get())[node_id];
+}
+
+double IGraph::betweenness(int node_id) const {
+  if (betweenness_.get()->empty()) {
+    igraph_vs_t *vs_t = (igraph_vs_t *)malloc(sizeof(igraph_vs_t));
+    igraph_vs_all(vs_t);
+    igraph_betweenness(graph_, betweenness_.get()->vector(), *vs_t, 0, NULL, 1);
+    delete vs_t;
+  }
+  return (*betweenness_.get())[node_id];
+}
+
+void IGraph::writeGML(std::string file_name) {
+  FILE* f = fopen(file_name.c_str(), "w");
+  igraph_write_graph_gml(graph_, f, NULL, "Iva");
+  fclose(f);
+}
+
+double IGraph::eigenvector_centrality(int node_id) const {
+  if (eigenvector_centrality_.get()->empty()) {
+    igraph_arpack_options_t *options =
+        (igraph_arpack_options_t *)malloc(sizeof(igraph_arpack_options_t));
+    igraph_arpack_options_init(options);
+    igraph_eigenvector_centrality(
+        graph_, eigenvector_centrality_.get()->vector(), NULL, 0 /* directed */,
+        1 /* scale */, NULL, options);
+    delete options;
+  }
+  return (*eigenvector_centrality_.get())[node_id];
+}
+
 IGraph *IGraph::GraphFromGML(const std::string &file_name) {
   igraph_t *graph = (igraph_t *)malloc(sizeof(igraph_t));
   FILE *f = fopen(file_name.c_str(), "r");
--- source_detection_seq.cc
+++ source_detection_seq.cc
@@ -7,6 +7,9 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
+#include <igraph/igraph.h>
 
 using cmplx::SourceDetector;
 using cmplx::common::IGraph;
@@ -15,19 +18,64 @@ using cmplx::common::SirParams;
 using cmplx::common::Realization;
 using cmplx::SourceDetectionParams;
 
+void generateBarabasi(int no, int poc = 0) {
+  for (int t = poc; t < poc + no; ++t) {
+    printf("%d\n", t);
+    std::string file_name = "barabasi2_900_" + std::to_string(t) + ".gml";
+    IGraph* g = IGraph::BarabasiAlbert(900);
+    g->writeGML(file_name);
+    std::string info_name = "barabasi2_900_" + std::to_string(t) + ".info";
+    FILE* f = fopen(info_name.c_str(), "w");
+    fprintf(f, "id,deg,kcore,clos,betw,eigcentr\n");
+    for (int i = 0; i < g->vertices(); ++i) {
+      fprintf(f, "%d,%d,%d,%.10lf,%.10lf,%.10lf\n", i, g->deg(i), g->kCore(i),
+              g->closeness(i), g->betweenness(i), g->eigenvector_centrality(i));
+    }
+    fclose(f);
+    delete (g);
+  }
+}
+
+void generateErdosRenyi(int no, int poc = 0) {
+  for (int t = poc; t < poc + no; ++t) {
+    int gs = 0;
+    while (true) {
+      printf("gs: %d\n", gs);
+      printf("%d\n", t);
+      IGraph* g = IGraph::ErdosRenyi(900, 0.01);
+      if (g->is_connected()) {
+        std::string file_name =
+            "erdos_renyi_900_0.01_" + std::to_string(t) + ".gml";
+        g->writeGML(file_name);
+        std::string info_name =
+            "erdos_renyi_900_0.01_" + std::to_string(t) + ".info";
+        FILE* f = fopen(info_name.c_str(), "w");
+        fprintf(f, "id,deg,kcore,clos,betw,eigcentr\n");
+        for (int i = 0; i < g->vertices(); ++i) {
+          fprintf(f, "%d,%d,%d,%.10lf,%.10lf,%.10lf\n", i, g->deg(i),
+                  g->kCore(i), g->closeness(i), g->betweenness(i),
+                  g->eigenvector_centrality(i));
+        }
+        fclose(f);
+        delete g;
+        break;
+      }
+      delete g;
+      gs++;
+    }
+  }
+}
+
 int main() {
-  auto params = SourceDetectionParams::SupFig2Params();
-  // auto params = SourceDetectionParams::ParamsFromGrid(0.5, 0.5, 5);
+  // generateBarabasi(50, 0);
+  generateErdosRenyi(50, 0);
 
-  cmplx::SequentialMCDetector sd(params->graph().get());
-  // std::vector<double> probs = sd.softMarginDetection(
-  //    params->realization(), params->simulations(), params->a());
+  // fclose(f);
 
-  std::vector<double> probs =
-      sd.seqMonteCarloDetectionSIR(params->realization(), 16000);
-
+  /*
   for (double p : probs) {
     printf("%.10lf\n", p);
   }
+  */
   return 0;
 }
