61de4e7e34f0c129402285ac0769180611bb845a
= <ivamiho@gmail.com>
2016-12-27 22:52:00 +0100

    Move openmp to new branch

--- openmp/omp_source_detection.h
+++
@@ -1,8 +0,0 @@
-#ifndef OMP_SOURCE_DETECTION_PARAL_H
-#define OMP_SOURCE_DETECTION_PARAL_H
-
-#include "omp_directmc.h"
-#include "omp_softmc.h"
-#include "omp_seqis.h"
-
-#endif  // OMP_SOURCE_DETECTION_PARAL_H
